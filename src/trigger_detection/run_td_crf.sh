2e480c40bfe89159d74fafaea0f609ec05908144
xusheng <jsksxs360@163.com>
2022-11-28 14:57:40 +0800

    add trigger detection models

---
+++ src/trigger_detection/run_td_crf.sh
@@ -0,0 +1,18 @@
+export OUTPUT_DIR=./longformer_cnn_crf_results/
+
+python3 run_td_crf.py \
+    --output_dir=$OUTPUT_DIR \
+    --model_type=longformer \
+    --model_checkpoint=../../PT_MODELS/allenai/longformer-large-4096/ \
+    --use_addition_layer=cnn \
+    --train_file=../../data/train_filtered.json \
+    --dev_file=../../data/dev_filtered.json \
+    --test_file=../../data/test_filtered.json \
+    --max_seq_length=4096 \
+    --learning_rate=1e-5 \
+    --crf_learning_rate=5e-5 \
+    --num_train_epochs=10 \
+    --batch_size=1 \
+    --do_train \
+    --warmup_proportion=0. \
+    --seed=42
