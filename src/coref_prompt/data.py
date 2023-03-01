86ebc1424a0c8efe92303064fd6bdd07d0249f5e
xusheng <jsksxs360@163.com>
2023-03-01 17:45:06 +0800

    update

--- src/coref_prompt/data.py
+++ src/coref_prompt/data.py
@@ -1,5 +1,7 @@
 from torch.utils.data import Dataset
 import json
+from tqdm.auto import tqdm
+from utils import create_prompt
 
 PROMPT_TYPE = [
     'hn', 'hm', 'hq', # base prompts 
@@ -18,13 +20,38 @@ EVENT_SUBTYPES = [ # 18 subtypes
 id2subtype = {idx: c for idx, c in enumerate(EVENT_SUBTYPES, start=1)}
 subtype2id = {v: k for k, v in id2subtype.items()}
 
+def get_pred_arguments(arg_file:str) -> dict:
+    '''
+    # Returns: 
+        - argument dictionary: {doc_id: {event_id: [{"global_offset": 798, "mention": "We", "role": "participant"}]}}
+    '''
+    participant_roles = set(['defendant', 'entity', 'person', 'position', 'agent', 'attacker', 
+                             'giver', 'victim', 'audience', 'recipient', 'target', 'seller', 
+                             'beneficiary', 'plaintiff', 'adjudicator', 'org', 'prosecutor'])
+    place_roles = set(['place', 'destination', 'origin'])
+    arg_dict = {}
+    with open(arg_file, 'rt', encoding='utf-8') as f:
+        for line in tqdm(f.readlines()):
+            sample = json.loads(line.strip())
+            arg_dict[sample['doc_id']] = {
+                event['start']: [
+                    {
+                        'global_offset': arg['start'], 
+                        'mention': arg['mention'], 
+                        'role': 'participant' if arg['role'].lower() in participant_roles else 'place'
+                    } for arg in event['arguments'] if arg['role'].lower() in participant_roles | place_roles
+                ] 
+                for event in sample['event_args']
+            }
+    return arg_dict
+
 class KBPCoref(Dataset):
     def __init__(self, data_file:str, arg_file:str, prompt_type:str, model_type:str, tokenizer, max_length:int):
         assert prompt_type in PROMPT_TYPE and model_type in ['bert', 'roberta', 'longformer']
         self.model_type = model_type
         self.tokenizer = tokenizer
         self.max_length = max_length
-        # self.kbp_entity_dict = get_KBP_entities(entity_file)
+        self.arg_dict = get_pred_arguments(arg_file)
         self.data = self.load_data(data_file, prompt_type)
         
     def _get_event_cluster_id(self, event_id:str, clusters:list) -> str:
@@ -36,7 +63,7 @@ class KBPCoref(Dataset):
     def load_data(self, data_file, prompt_type:str):
         Data = []
         with open(data_file, 'rt', encoding='utf-8') as f:
-            for line in f:
+            for line in tqdm(f.readlines()):
                 sample = json.loads(line.strip())
                 clusters = sample['clusters']
                 sentences = sample['sentences']
@@ -48,34 +75,27 @@ class KBPCoref(Dataset):
                         event_1, event_2 = events[i], events[j]
                         event_1_cluster_id = self._get_event_cluster_id(event_1['event_id'], clusters)
                         event_2_cluster_id = self._get_event_cluster_id(event_2['event_id'], clusters)
-                        
-                        
-                        
-                        new_event_sent = create_new_sent(
+                        prompt_data = create_prompt(
                             event_1['sent_idx'], event_1['sent_start'], event_1['trigger'], 
                             event_2['sent_idx'], event_2['sent_start'], event_2['trigger'], 
-                            sentences, sentences_lengths, sentences_entities, 
-                            special_token_dict, context_k, max_length, self.tokenizer
+                            sentences, sentences_lengths, 
+                            prompt_type, self.model_type, self.tokenizer, self.max_length
                         )
                         Data.append({
                             'id': sample['doc_id'], 
-                            'sent': new_event_sent['new_sent'], 
-                            'e1_offset': event_1['start'], # event1
-                            'e1_trigger': new_event_sent['e1_trigger'], 
-                            'e1_subtype': subtype2id.get(event_1['subtype'], 0), # 0 - 'other'
-                            'e1_subtype_str': event_1['subtype'] if event_1['subtype'] in SUBTYPES else 'normal', 
-                            'e1_start': new_event_sent['e1_sent_start'], 
-                            'e1s_start': new_event_sent['e1s_sent_start'], 
-                            'e1e_start': new_event_sent['e1e_sent_start'], 
-                            'e1_entities': new_event_sent['e1_entities'], 
-                            'e2_offset': event_2['start'], # event2
-                            'e2_trigger': new_event_sent['e2_trigger'], 
-                            'e2_subtype': subtype2id.get(event_2['subtype'], 0), # 0 - 'other'
-                            'e2_subtype_str': event_2['subtype'] if event_2['subtype'] in SUBTYPES else 'normal', 
-                            'e2_start': new_event_sent['e2_sent_start'], 
-                            'e2s_start': new_event_sent['e2s_sent_start'], 
-                            'e2e_start': new_event_sent['e2e_sent_start'], 
-                            'e2_entities': new_event_sent['e2_entities'], 
+                            'prompt': prompt_data['prompt'], 
+                            'e1_id': event_1['start'], # event1
+                            'e1_trigger': event_1['trigger'], 
+                            'e1_subtype': event_1['subtype'] if event_1['subtype'] in EVENT_SUBTYPES else 'normal', 
+                            'e1_subtype_id': subtype2id.get(event_1['subtype'], 0), # 0 - 'other'
+                            'e1s_offset': prompt_data['e1s_offset'], 
+                            'e1e_offset': prompt_data['e1e_offset'], 
+                            'e2_id': event_2['start'], # event2
+                            'e2_trigger': event_2['trigger'], 
+                            'e2_subtype': event_2['subtype'] if event_2['subtype'] in EVENT_SUBTYPES else 'normal', 
+                            'e2_subtype_id': subtype2id.get(event_2['subtype'], 0), # 0 - 'other'
+                            'e2s_offset': prompt_data['e2s_offset'], 
+                            'e2e_offset': prompt_data['e2e_offset'], 
                             'label': 1 if event_1_cluster_id == event_2_cluster_id else 0
                         })
         return Data
@@ -85,3 +105,44 @@ class KBPCoref(Dataset):
 
     def __getitem__(self, idx):
         return self.data[idx]
+
+if __name__ == '__main__':
+
+    def print_data_statistic(data_file):
+        doc_list = []
+        with open(data_file, 'rt', encoding='utf-8') as f:
+            for line in f:
+                doc_list.append(json.loads(line.strip()))
+        doc_num = len(doc_list)
+        event_num = sum([len(doc['events']) for doc in doc_list])
+        cluster_num = sum([len(doc['clusters']) for doc in doc_list])
+        singleton_num = sum([1 if len(cluster['events']) == 1  else 0 
+                                    for doc in doc_list for cluster in doc['clusters']])
+        print(f"Doc: {doc_num} | Event: {event_num} | Cluster: {cluster_num} | Singleton: {singleton_num}")
+
+    arg_dict = get_pred_arguments('../../data/EventExtraction/omni_gold_test_pred_args.json')
+    for event_arg_dic in arg_dict.values():
+        print(event_arg_dic)
+    # from transformers import AutoTokenizer
+    # import argparse
+    # parser = argparse.ArgumentParser()
+    # args = parser.parse_args()
+    # args.batch_size = 4
+    # args.max_seq_length = 512
+    # args.model_type = 'longformer'
+    # args.model_checkpoint = '../../PT_MODELS/allenai/longformer-large-4096'
+
+    # tokenizer = AutoTokenizer.from_pretrained(args.model_checkpoint)
+    # special_tokens_dict = {'additional_special_tokens': ['<e1_start>', '<e1_end>', '<e2_start>', '<e2_end>', '<l1>', '<l2>', '<l3>', '<l4>', '<l5>', '<l6>']}
+    # tokenizer.add_special_tokens(special_tokens_dict)
+
+    # train_data = KBPCoref('../../data/train_filtered.json', '../../data/kbp_sent_entity.txt', prompt_type='sn', model_type='longformer', tokenizer=tokenizer, max_length=482)
+    # print_data_statistic('../../data/train_filtered.json')
+    # print(len(train_data))
+    # labels = [train_data[s_idx]['label'] for s_idx in range(len(train_data))]
+    # print('Coref:', labels.count(1), 'non-Coref:', labels.count(0))
+    # for i in range(5):
+    #     print(train_data[i])
+    # print('Testing dataset...')
+    # for _ in tqdm(train_data):
+    #     pass
--- src/coref_prompt/utils.py
+++ src/coref_prompt/utils.py
@@ -1,4 +1,3 @@
-from typing import Tuple
 import numpy as np
 
 def create_event_context(
@@ -7,8 +6,8 @@ def create_event_context(
     sentences:list, sentence_lens:list, 
     s_tokens:dict, tokenizer, max_length:int
     ) -> dict:
-    assert e1_sent_start < e2_sent_start
     if e1_sent_idx == e2_sent_idx: # two events in the same sentence
+        assert e1_sent_start < e2_sent_start
         e1_e2_sent = sentences[e1_sent_idx]['text']
         core_context = f"{e1_e2_sent[:e1_sent_start]}"
         e1s_offset = len(core_context)
@@ -25,15 +24,15 @@ def create_event_context(
         while True:
             if e_before >= 0:
                 if total_length + sentence_lens[e_before] <= max_length:
-                    core_context = sentences[e_before] + ' ' + core_context
-                    e1s_offset, e1e_offset, e2s_offset, e2e_offset = np.asarray([e1s_offset, e1e_offset, e2s_offset, e2e_offset]) + np.full((4,), len(sentences[e_before]) + 1)
+                    core_context = sentences[e_before]['text'] + ' ' + core_context
+                    e1s_offset, e1e_offset, e2s_offset, e2e_offset = np.asarray([e1s_offset, e1e_offset, e2s_offset, e2e_offset]) + np.full((4,), len(sentences[e_before]['text']) + 1)
                     total_length += 1 + sentence_lens[e_before]
                     e_before -= 1
                 else:
                     e_before = -1
             if e_after < len(sentences):
                 if total_length + sentence_lens[e_after] <= max_length:
-                    core_context += ' ' + sentences[e_after]
+                    core_context += ' ' + sentences[e_after]['text']
                     total_length += 1 + sentence_lens[e_after]
                     e_after += 1
                 else:
@@ -72,30 +71,30 @@ def create_event_context(
             e1_after_dead, e2_before_dead = False, False
             if e1_before >= 0:
                 if total_length + sentence_lens[e1_before] <= max_length:
-                    e1_context = sentences[e1_before] + ' ' + e1_context
-                    e1s_offset, e1e_offset = np.asarray([e1s_offset, e1e_offset]) + np.full((2,), len(sentences[e1_before]) + 1)
+                    e1_context = sentences[e1_before]['text'] + ' ' + e1_context
+                    e1s_offset, e1e_offset = np.asarray([e1s_offset, e1e_offset]) + np.full((2,), len(sentences[e1_before]['text']) + 1)
                     total_length += 1 + sentence_lens[e1_before]
                     e1_before -= 1
                 else:
-                    e_before = -1
+                    e1_before = -1
             if e1_after <= e2_before:
                 if total_length + sentence_lens[e1_after] <= max_length:
-                    e1_context += ' ' + sentences[e1_after]
+                    e1_context += ' ' + sentences[e1_after]['text']
                     total_length += 1 + sentence_lens[e1_after]
                     e1_after += 1
                 else:
                     e1_after_dead = True
             if e2_before >= e1_after:
                 if total_length + sentence_lens[e2_before] <= max_length:
-                    e2_context = sentences[e2_before] + ' ' + e2_context
-                    e2s_offset, e2e_offset = np.asarray([e2s_offset, e2e_offset]) + np.full((2,), len(sentences[e2_before]) + 1)
+                    e2_context = sentences[e2_before]['text'] + ' ' + e2_context
+                    e2s_offset, e2e_offset = np.asarray([e2s_offset, e2e_offset]) + np.full((2,), len(sentences[e2_before]['text']) + 1)
                     total_length += 1 + sentence_lens[e2_before]
                     e2_before -= 1
                 else:
                     e2_before_dead = True
             if e2_after < len(sentences):
                 if total_length + sentence_lens[e2_after] <= max_length:
-                    e2_context += ' ' + sentences[e2_after]
+                    e2_context += ' ' + sentences[e2_after]['text']
                     total_length += 1 + sentence_lens[e2_after]
                     e2_after += 1
                 else:
@@ -116,7 +115,7 @@ def create_event_context(
         }
 
 def create_base_template(e1_trigger:str, e2_trigger:str, prompt_type:str, s_tokens:dict) -> dict:
-    if prompt_type.startswith('h'):
+    if prompt_type.startswith('h'): # hard template
         if prompt_type == 'hn':
             template = f"In the following text, events expressed by {s_tokens['e1s']} {e1_trigger} {s_tokens['e1e']} and {s_tokens['e2s']} {e2_trigger} {s_tokens['e2e']} refer to {s_tokens['mask']} event: "
         elif prompt_type == 'hm':
@@ -126,7 +125,7 @@ def create_base_template(e1_trigger:str, e2_trigger:str, prompt_type:str, s_toke
         else:
             raise ValueError(f'Unknown prompt type: {prompt_type}')
         normal_s_tokens = [s_tokens['e1s'], s_tokens['e1e'], s_tokens['e2s'], s_tokens['e2e']]
-    elif prompt_type.startswith('h'):
+    elif prompt_type.startswith('s'): # soft template
         if prompt_type == 'sn':
             template = f"In the following text, {s_tokens['l1']} {s_tokens['e1s']} {e1_trigger} {s_tokens['e1e']} {s_tokens['l2']} {s_tokens['l3']} {s_tokens['e2s']} {e2_trigger} {s_tokens['e2e']} {s_tokens['l4']} {s_tokens['l5']} {s_tokens['mask']} {s_tokens['l6']}: "
             normal_s_tokens = [s_tokens['e1s'], s_tokens['e1e'], s_tokens['e2s'], s_tokens['e2e'], s_tokens['l1'], s_tokens['l2'], s_tokens['l3'], s_tokens['l4'], s_tokens['l5'], s_tokens['l6']]
@@ -151,23 +150,18 @@ def create_prompt(
     e2_sent_idx:int, e2_sent_start:int, e2_trigger:str,  
     sentences:list, sentence_lens:list, 
     prompt_type:str, model_type, tokenizer, max_length:int
-    ) -> Tuple[dict, dict]:
+    ) -> dict:
 
     special_token_dict = {
-        'e1s': '[E1_START]', 'e1e': '[E1_END]', 'e2s': '[E2_START]', 'e2e': '[E2_END]', 
-        'l1': '[L1]', 'l2': '[L2]', 'l3': '[L3]', 'l4': '[L4]', 'l5': '[L5]', 'l6': '[L6]', 
-        'e1_arg': '[L7]', 'e2_arg': '[L8]', 
-        'mask': '[MASK]'
+        'mask': '[MASK]', 'e1s': '[E1_START]', 'e1e': '[E1_END]', 'e2s': '[E2_START]', 'e2e': '[E2_END]', 
+        'l1': '[L1]', 'l2': '[L2]', 'l3': '[L3]', 'l4': '[L4]', 'l5': '[L5]', 'l6': '[L6]'
     } if model_type == 'bert' else {
-        'e1s': '<e1_start>', 'e1e': '<e1_end>', 'e2s': '<e2_start>', 'e2e': '<e2_end>', 
-        'l1': '<l1>', 'l2': '<l2>', 'l3': '<l3>', 'l4': '<l4>', 'l5': '<l5>', 'l6': '<l6>', 
-        'e1_arg': '<l7>', 'e2_arg': '<l8>', 
-        'mask': '<mask>'
+        'mask': '<mask>', 'e1s': '<e1_start>', 'e1e': '<e1_end>', 'e2s': '<e2_start>', 'e2e': '<e2_end>', 
+        'l1': '<l1>', 'l2': '<l2>', 'l3': '<l3>', 'l4': '<l4>', 'l5': '<l5>', 'l6': '<l6>'
     }
 
     if prompt_type.startswith('h') or prompt_type.startswith('s'): # base prompt
         template_data = create_base_template(e1_trigger, e2_trigger, prompt_type, special_token_dict)
-
         assert set(template_data['sprcial_tokens']).issubset(set(tokenizer.additional_special_tokens))
         context_data = create_event_context(
             e1_sent_idx, e1_sent_start, e1_trigger, 
@@ -175,7 +169,26 @@ def create_prompt(
             sentences, sentence_lens, 
             special_token_dict, tokenizer, max_length - len(tokenizer(template_data['template']).tokens()) - 1
         )
-
-    # assert special_token
-
+        e1s_offset, e1e_offset, e2s_offset, e2e_offset = context_data['e1s_offset'], context_data['e1e_offset'], context_data['e2s_offset'], context_data['e2e_offset']
+        if 'e1_e2_context' in context_data: # two events in the same sentence
+            prompt = template_data['template'] + context_data['e1_e2_context']
+            e1s_offset, e1e_offset, e2s_offset, e2e_offset = np.asarray([e1s_offset, e1e_offset, e2s_offset, e2e_offset]) + np.full((4,), len(template_data['template']))
+        else: # two events in different sentences
+            prompt = template_data['template'] + context_data['e1_context'] + ' ' + context_data['e2_context']
+            e1s_offset, e1e_offset = np.asarray([e1s_offset, e1e_offset]) + np.full((2,), len(template_data['template']))
+            e2s_offset, e2e_offset = np.asarray([e2s_offset, e2e_offset]) + np.full((2,), len(template_data['template']) + len(context_data['e1_context']) + 1)
+        mask_offset = template_data['template'].find(special_token_dict['mask'])
+        assert prompt[mask_offset:mask_offset + len(special_token_dict['mask'])] == special_token_dict['mask']
+        assert prompt[e1s_offset:e1e_offset] == special_token_dict['e1s'] + ' ' + e1_trigger + ' '
+        assert prompt[e1e_offset:e1e_offset + len(special_token_dict['e1e'])] == special_token_dict['e1e']
+        assert prompt[e2s_offset:e2e_offset] == special_token_dict['e2s'] + ' ' + e2_trigger + ' '
+        assert prompt[e2e_offset:e2e_offset + len(special_token_dict['e2e'])] == special_token_dict['e2e']
+        return {
+            'prompt': prompt, 
+            'mask_offset': mask_offset, 
+            'e1s_offset': e1s_offset, 
+            'e1e_offset': e1e_offset, 
+            'e2s_offset': e2s_offset, 
+            'e2e_offset': e2e_offset
+        }
 
